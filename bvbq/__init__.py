122fbaa535565e2dcf41b3ee81767d34eb9c4f04
Danilo Naiff <danilonaiff1992@gmail.com>
2021-08-11 17:06:49 -0300

    Remove old directories

--- bvbq/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import distributions,gp,kernelfunctions,\
-              bvi,utils,optwrapper,bq,bvbq_functions
--- bvbq_jax/distributions_utils.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import jax
-import jax.numpy as jnp
