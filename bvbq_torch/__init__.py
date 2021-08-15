66748eb34a0f63ae357e762f1c7a0bc9a362721a
Danilo Naiff <danilonaiff1992@gmail.com>
2021-08-14 22:33:26 -0300

    Remove bvbq_torch_directory and changed to bvbq

--- bvbq_torch/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    This will be the main documentation
-"""
-from . import gp, kernel_functions, distributions, bayesquad, utils,\
-    bvbq_functions, bvbq, acquisition_functions, metrics
-from .named_interface import BVBQNamedMixMVN
